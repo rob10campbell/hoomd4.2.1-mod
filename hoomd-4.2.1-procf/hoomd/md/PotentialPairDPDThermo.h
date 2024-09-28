4be9b728c129ded755a02b0be8e6d220e0459bb8
rob10campbell <88801278+rob10campbell@users.noreply.github.com>
2024-09-28 17:08:23 -0400

    remove virial double-count in DPDThermo

--- hoomd-4.2.1-procf/hoomd/md/PotentialPairDPDThermo.h
+++ hoomd-4.2.1-procf/hoomd/md/PotentialPairDPDThermo.h
@@ -214,9 +214,6 @@ template<class evaluator> void PotentialPairDPDThermo<evaluator>::computeForces(
         Scalar viriali[6];
         for (unsigned int l = 0; l < 6; l++)
             viriali[l] = 0.0;
-        //~ initialize virialxyi_ind to zero [RHEOINF]
-        Scalar virialxyi_ind = 0.0;
-        //~
         //~ initialize the current virial_ind to zero [RHEOINF]
         Scalar viriali_ind[5];
         for (unsigned int l = 0; l < 5; l++)
@@ -363,10 +360,6 @@ template<class evaluator> void PotentialPairDPDThermo<evaluator>::computeForces(
                 pair_virial[4] = Scalar(0.5) * dx.y * dx.z * force_divr_cons;
                 pair_virial[5] = Scalar(0.5) * dx.z * dx.z * force_divr_cons;
 
-                //~ compute virialxyi_ind [RHEOINF]
-                virialxyi_ind += Scalar(0.5) * force_divr_cons * dx.x * dx.y;
-                //~
-
                 //~ compute the virial_ind [RHEOINF]
 		Scalar virial_ind_prefix = Scalar(0.5) * dx.x * dx.y;
 		Scalar pair_virial_ind[5];
@@ -399,9 +392,6 @@ template<class evaluator> void PotentialPairDPDThermo<evaluator>::computeForces(
                     h_force.data[mem_idx].w += pair_eng * Scalar(0.5);
                     for (unsigned int l = 0; l < 6; l++)
                         h_virial.data[l * this->m_virial_pitch + mem_idx] += pair_virial[l];
-                    //~ add virialxyi_ind [RHEOINF]
-                    h_virial_ind.data[0 * this->m_virial_ind_pitch + mem_idx] += Scalar(0.5) * force_divr_cons * dx.x * dx.y;
-                    //~
                     //~ add virial_ind [RHEOINF] 
                     for (unsigned int l = 0; l < 5; l++)
                         h_virial_ind.data[l * this->m_virial_ind_pitch + mem_idx] += pair_virial_ind[l];
@@ -418,9 +408,6 @@ template<class evaluator> void PotentialPairDPDThermo<evaluator>::computeForces(
         h_force.data[mem_idx].w += pei;
         for (unsigned int l = 0; l < 6; l++)
             h_virial.data[l * this->m_virial_pitch + mem_idx] += viriali[l];
-        //~ add virialxyi_ind [RHEOINF]
-        h_virial_ind.data[0 * this->m_virial_ind_pitch + mem_idx] += virialxyi_ind;
-        //~
         //~ add virial_ind [RHEOINF] 
         for (unsigned int l = 0; l < 5; l++)
             h_virial_ind.data[l * this->m_virial_ind_pitch + mem_idx] += viriali_ind[l];
